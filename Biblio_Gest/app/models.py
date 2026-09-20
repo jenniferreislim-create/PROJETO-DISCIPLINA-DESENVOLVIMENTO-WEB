4e3d7808081a3b4dbadc3572c971f6bfbaa511e6
jenniferreislim-create <jenniferreislim@gmail.com>
2026-09-20 11:39:38 -0300

    instruções básicas para execução

--- Biblio_Gest/app/models.py
+++ Biblio_Gest/app/models.py
@@ -5,13 +5,15 @@ class Usuário(models.Model):
     nome = models.CharField(max_length=100)
     vinculo = models.CharField(max_length=10)
 
-
+    def __str__(self):
+        return self.nome
 
 
 class Empréstimo(models.Model):
     livros= models.CharField(max_length=50)
     dataEmprestimo = models.DateField()
     dataDevolução = models.DateField()
+
 
 class Livro(models.Model):
     nome = models.CharField(max_length=100)
